bae1a08b984ff52cb7d249f3973c5a7e4d7d3ad5
Alex Hepburn <ah13558@it000526.home>
2018-05-17 13:26:57 +0100

    Made the list of songs over multiple columns, added the most recommended 5 songs to user

--- flask_app.py
+++ flask_app.py
@@ -3,9 +3,10 @@ from flask import render_template, request
 import pandas as pd
 import numpy as np 
 
-num_songs = 30 # controls how many songs are in the dataset
+num_songs = 100 # controls how many songs are in the dataset
 x = np.load('./x.npy')[:, 0:num_songs]
 z = np.load('./z.npy')[:, 0:num_songs]
+znorm = np.load('./znorm.npy')[:, 0:num_songs]
 S = np.where(x!=0, 1, 0)
 df = pd.read_hdf('./flask_app_df.h5', 'df').head(num_songs)
 app = Flask(__name__)
@@ -17,16 +18,19 @@ def app2(a,A):
 
 def get_recommendation(songs):
     idx = [df.index[df.artsong==x][0] for x in songs]
-    print(len(idx))
     if len(idx) == 0:
         return df.sample(1).iloc[0].artsong
     new_user = np.ones((len(idx),), dtype=int)
     S_temp = S[:, idx]
     rec = z[app2(new_user, S_temp), :]
-    return df.ix[rec.argmax()].artsong
+    rec2 = znorm[app2(new_user, S_temp), :]
+    rec[idx] = 0
+    rec2[idx] = 0
+    return (df.ix[rec.argmax()].artsong, df.ix[rec2.argmax()].artsong,
+        list(df.ix[(-rec).argsort()[:5]].artsong), list(df.ix[(-rec2).argsort()[:5]].artsong))
 
 def get_dropdown_list():
-    return list(df.artsong)
+    return sorted(list(df.artsong))
 
 def get_youtube_link(artsong):
     row = df.loc[df.artsong == artsong]
@@ -41,8 +45,11 @@ def login():
     if request.method == "POST":
         songs = request.form.getlist('songselect')
         if songs!=None:
-            song = get_recommendation(songs)
-            return render_template("index.html", songs=get_dropdown_list(), youtube_id=get_youtube_link(song))
+            song, song2, rec, rec2 = get_recommendation(songs)
+            print(rec)
+            print(rec2)
+            return render_template("index.html", songs=get_dropdown_list(), youtube_id=get_youtube_link(song), 
+                youtube_idtwo=get_youtube_link(song2), rec=rec, rec2=rec2)
     return render_template("index.html", songs=get_dropdown_list())
 
 if __name__ == "__main__":
